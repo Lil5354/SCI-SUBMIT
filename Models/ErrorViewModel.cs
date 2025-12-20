c81c7665c8bb860712945b75778d305e4a6c70a9
Lil5354 <dttthao.5354@gmail.com>
2025-12-20 12:25:26 +0700

    Initial commit: SciSubmit ASP.NET Core MVC project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SciSubmit.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
